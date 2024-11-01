d1e4706c927cb47905f7b3800bf79ef1a8f7d282
Aqau3k <c3ryumon@gmail.com>
2024-11-01 21:33:04 +0900

    コメントを追加

--- simulateda_annealing.py
+++ simulateda_annealing.py
@@ -9,13 +9,35 @@ Score = int|float
 
 class SimulatedaAnnealingTemplate:
     def __init__(self, initial_status: Status, initial_score: Score):
+        """コンストラクタ
+
+        Args:
+            initial_status (Status): 解の初期状態
+            initial_score (Score): 初期解の評価値
+        """
         self.initial_status = initial_status
         self.initial_score = initial_score
 
     def should_terminate(self) -> bool:
+        """焼きまなしを終了するか判定する
+
+        焼きなましの各ループでコールされる
+
+        Returns:
+            bool: True(終了)/False(続行)
+        """
         pass
 
     def optimize(self, temperature: int|float, rate: float) -> Status:
+        """焼きなましを実行して解を求める
+
+        Args:
+            temperature (int | float): 温度
+            rate (float): 温度を下げる割合、毎ループ温度に乗算される
+
+        Returns:
+            Status: 見つかった解
+        """
         score = self.initial_score
         best_score = score
 
@@ -42,8 +64,26 @@ class SimulatedaAnnealingTemplate:
         return best_status
 
     def dry_run(self, status: Status, score: Score) -> tuple[Diff, Score]:
+        """何かの操作を仮実行した場合の評価を行う
+
+        Args:
+            status (Status): 現在の解の状態
+            score (Score): 現在のスコア
+
+        Returns:
+            tuple[Diff, Score]: 差分情報, 操作を実行した場合のスコア
+        """
         pass
 
     def operate(self, status: Status, diff: Diff) -> Status:
+        """実際にDiffに対する操作を行う
+
+        Args:
+            status (Status): 現在の解の状態
+            diff (Diff): 差分情報(dry_runで作ったやつ)
+
+        Returns:
+            Status: 操作後の解の状態
+        """
         pass
 
