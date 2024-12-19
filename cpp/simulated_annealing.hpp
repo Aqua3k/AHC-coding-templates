212ad41a99372610606047169bbf554c674f7946
Aqau3k <c3ryumon@gmail.com>
2024-12-19 21:21:59 +0900

    一応実装した

--- cpp/simulated_annealing.hpp
+++ cpp/simulated_annealing.hpp
@@ -27,7 +27,33 @@ public:
     }
 
     Status optimize(double temperature, double rate) {
-        
+        auto score = initial_score;
+        auto best_score = score;
+
+        auto status = initial_status;
+        auto best_status = status;
+
+        while(!should_terminate()) {
+            auto result = dry_run();   
+            auto diff = get<0>(result);
+            auto new_score = get<1>(result);
+            auto t = (score - new_score) / temperature;
+            bool swap = true;
+            if (t < 1.0) {
+                swap = static_cast<double>(rand()) / RAND_MAX < exp(t);
+            }
+
+            if (swap) {
+                auto status = oeprate(status, diff);
+                score = new_score;
+                if (score < best_score) {
+                    best_score = score;
+                    best_status = status;
+                }
+            }
+            temperature *= rate;
+        }
+        return best_status;
     }
 
     virtual tuple<Diff, Score> dry_run() {}
